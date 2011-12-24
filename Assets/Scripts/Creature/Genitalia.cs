2669750eedf4b91dbc86dc045fcf6995e88c6189
Craig Lomax <c.lomax.uk@gmail.com>
2011-12-24 20:32:25 +0000

    Added material for genital vector

--- Assets/Scripts/Creature/Genitalia.cs
+++ Assets/Scripts/Creature/Genitalia.cs
@@ -13,16 +13,69 @@ using System.Collections;
 
 public class Genitalia : MonoBehaviour {
 #pragma warning disable 0414
+	public Creature crt;
 	private Logger lg;
-	private Creature crt;
-	private Creature other_crt;
 	private Spawner spw;
+	private Transform _t;
+	private LineRenderer lr;
+	private Vector3 line_start;
+	private float line_length = 0.5F;
+	private Vector3 line_end;
+	private float line_width = 0.5F;
+	private int crt_detect_range = 2000;
 #pragma warning restore 0414
 
 	void Start () {
+		this.gameObject.tag = "Genital";
 		lg = Logger.getInstance();
-		crt = transform.parent.GetComponent<Creature>();
 		spw = Spawner.getInstance();
+		
+		_t = transform;
+		lr = (LineRenderer)this.gameObject.AddComponent("LineRenderer");
+		lr.material = (Material)Resources.Load("Materials/genital_vector");
+		lr.SetWidth(line_width, line_width);
+		lr.SetVertexCount(2);
+		lr.castShadows = false;
+		lr.receiveShadows = false;
+		lr.renderer.enabled = true;
+	}
+	
+	void Update () {
+		GameObject cc = closestCreature();
+		if(cc) {
+			lr.useWorldSpace = true;
+			line_end = new Vector3(cc.transform.position.x, cc.transform.position.y, cc.transform.position.z);
+			line_start = _t.position;
+			lr.SetPosition(1,line_end);
+			resetStart();
+		} else {
+			lr.useWorldSpace = false;
+			line_start = new Vector3(0,0,0);
+			line_end = new Vector3(0,0,line_length);
+			lr.SetPosition(0,line_start);
+			lr.SetPosition(1,line_end);
+		}
+	}
+	
+	private GameObject closestCreature () {
+		GameObject[] crts = GameObject.FindGameObjectsWithTag("Genital");
+		GameObject closest = null;
+		float dist = crt_detect_range;
+		Vector3 pos = transform.position;
+		foreach(GameObject crt in crts) {
+			Vector3 diff = crt.transform.position - pos;
+			float curr_dist = diff.sqrMagnitude;
+			if (curr_dist < dist) {
+				closest = crt;
+				dist = curr_dist;
+			}
+		}
+		return closest;	
+	}
+	
+	private void resetStart () {
+		line_start = new Vector3(_t.position.x,_t.position.y,_t.position.z);
+		lr.SetPosition(0,line_start);
 	}
 	
 	/*
