418a22f25a1ab7e9b106efe341ccdb9209ba4d7e
Craig Lomax <craig@clomax.me.uk>
2013-07-13 21:32:15 +0100

    Replace SimpleJSON with LitJson.

--- Assets/Scripts/Util/Settings.cs
+++ Assets/Scripts/Util/Settings.cs
@@ -1,22 +1,22 @@
 using UnityEngine;
 using System.Collections;
 using System.IO;
-using SimpleJSON;
+using LitJson;
 
 public class Settings : MonoBehaviour {
 	
 	string settings_file = "settings.json";
 	StreamReader sr;
 	string _contents;
-	public JSONNode contents;
+	public JsonData contents;
 	
 	public static GameObject container;
 	public static Settings instance;
 	
 	public Settings () {
 		sr = new StreamReader(Application.dataPath + "/" + settings_file);
-		string _contents = sr.ReadToEnd();
-		contents = JSON.Parse(_contents);
+		_contents = sr.ReadToEnd();
+		contents = JsonMapper.ToObject(_contents);
 		sr.Close();
 	}
 	
