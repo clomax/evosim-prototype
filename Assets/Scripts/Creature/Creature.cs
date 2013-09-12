5df053eda3d183032fff0ea2522e7c9f4f4d5033
Craig Lomax <craig@clomax.me.uk>
2013-09-12 11:41:43 +0100

    Spread creatures out more at start.

--- Assets/Scripts/Creature/Creature.cs
+++ Assets/Scripts/Creature/Creature.cs
@@ -171,12 +171,10 @@ public class Creature : MonoBehaviour {
 				limb_script = limb_child.AddComponent<Limb>();
 				limb_script.parent = limb;
 				
-				/*
 				limb_script.setColour		( (Color)	l[0] );
 				limb_child.transform.parent = limb.transform;
 				limb_script.setPosition		( new Vector3(0,0,limb.transform.localPosition.z) / 2 );
 				limb_child.transform.parent = _t;
-				
 				limb_script.setScale		( new Vector3(1.6F,1.6F,4.4F) );
 				limb_script.setRecurrances	( 0 );
 				
