712a2b41086c5aba827ea34f2b6ce71a3a5c3083
Craig Lomax <craig@clomax.me.uk>
2014-05-12 17:48:38 +0100

    Added FPS display.

---
+++ Assets/Scripts/GUI/FPS.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPS : MonoBehaviour {
+
+	float fps;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("printFPS", 0F, 0.5F);
+	}
+
+	void Update () {
+		fps = 1.0f / Time.deltaTime;
+	}
+
+	void printFPS () {
+		guiText.text = "FPS: " + fps.ToString("F0");
+	}
+}
